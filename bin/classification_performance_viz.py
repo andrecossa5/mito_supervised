3f0699a8b131101a5913d35fbce5bd8537bb0531
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2023-05-05 14:59:58 +0200

    adding top models and script for classification performance viz

--- bin/classification_performance_viz.py
+++ bin/classification_performance_viz.py
@@ -4,6 +4,7 @@ Visualization of clones and samples classification performances.
 
 # Code
 import os
+from pysam import VariantFile
 from scipy.interpolate import interp1d 
 from mito_utils.preprocessing import *
 from mito_utils.diagnostic_plots import *
@@ -11,6 +12,7 @@ from mito_utils.heatmaps_plots import *
 from mito_utils.utils import *
 from mito_utils.plotting_base import *
 from matplotlib.gridspec import GridSpec
+import matplotlib
 matplotlib.use('macOSX')
 
 
@@ -161,6 +163,7 @@ box(
     'score',
     by='metric', 
     c=colors, 
+    hue_order=colors.keys(),
     ax=ax1, 
     kwargs=params
 )
@@ -188,7 +191,8 @@ box(
     'tuning', 
     'score',
     by='metric', 
-    c=colors, 
+    c=colors,
+    hue_order=colors.keys(),
     ax=ax2, 
     kwargs=params
 )
@@ -208,6 +212,7 @@ box(
     'score',
     by='metric', 
     c=colors, 
+    hue_order=colors.keys(),
     ax=ax3, 
     kwargs=params
 )
@@ -411,11 +416,9 @@ df_ = (
     .reset_index()
 )
 
-bar(df_, x='sample', y='ncells', c='lightgrey', ax=axs[0], s=0.75)
-format_ax(axs[0], title='n cells', ylabel='n', xticks=df_['sample'], yticks=[])
-axs[0].spines['top'].set_visible(False)
-axs[0].spines['left'].set_visible(False)
-axs[0].spines['right'].set_visible(False)
+bar(df_, x='sample', y='ncells', c='lightgrey', edgecolor='k', ax=axs[0], s=0.75)
+format_ax(axs[0], title='n cells', ylabel='n', xticks=df_['sample'])
+axs[0].spines[['top', 'right', 'left']].set_visible(False)
 
 # Center
 df_ = (
@@ -426,11 +429,9 @@ df_ = (
     .reset_index()
 )
 
-bar(df_, x='sample', y='n_clones_analyzed', c='lightgrey', ax=axs[1], s=0.75)
-format_ax(axs[1], title='n clones', xticks=df_['sample'], yticks=[])
-axs[1].spines['top'].set_visible(False)
-axs[1].spines['left'].set_visible(False)
-axs[1].spines['right'].set_visible(False)
+bar(df_, x='sample', y='n_clones_analyzed', c='lightgrey', edgecolor='k', ax=axs[1], s=0.75)
+format_ax(axs[1], title='n clones', xticks=df_['sample'])
+axs[1].spines[['top', 'right', 'left']].set_visible(False)
 
 # Right
 df_ = (
@@ -457,8 +458,7 @@ add_legend(
     label_size=10,
     ticks_size=8
 )
-axs[2].spines['top'].set_visible(False)
-axs[2].spines['right'].set_visible(False)
+axs[2].spines[['top', 'right', 'left']].set_visible(False)
 
 # Save
 fig.tight_layout()
@@ -477,43 +477,6 @@ fig.savefig(
 ############## Overall performance by sample 2: relationship with clonal complexity
 
 fig, axs = plt.subplots(1,3,figsize=(15,5))
-
-# Left
-# df_ = clones.assign(feature_type=lambda x: x['filtering'] + '_' + x['dimred'])
-# clones_order = (
-#     df_.groupby('comparison')['f1']
-#     .agg('mean')
-#     .sort_values(ascending=False)
-#     .index
-# )
-# good_clones = df_.query('f1>=0.7')['comparison'].unique()
-# df_['clone_status'] = np.where(df_['comparison'].isin(good_clones), 
-#                                 '>=1 good models', 'No good models')
-# 
-# df = df_
-# df['sample'] = pd.Categorical(df['sample']).remove_unused_categories()
-# df['clone_status'] = pd.Categorical(df['clone_status']).remove_unused_categories()
-# data = pd.crosstab(df['sample'], df['clone_status'], normalize='index')
-# data = data.sort_values('>=1 good models')
-# data_cum = data.cumsum(axis=1)
-# 
-# ys = data.index.categories
-# labels = data.columns.categories
-# 
-# colors = {'>=1 good models':'b', 'No good models':'lightblue'}
-# for i, x in enumerate(labels):
-#     widths = data.values[:,i]
-#     starts = data_cum.values[:,i] - widths
-#     axs[0].barh(ys, widths, left=starts, height=0.95, label=x, color=colors[x])
-#     
-# # Format
-# axs[0].set_xlim(-0.01, 1.01)
-# format_ax(axs[0], xlabel='Frequency %')
-# add_legend(label='clone_status', colors=colors, ax=axs[0], only_top=10, ncols=2,
-#     loc='lower left', bbox_to_anchor=(.18, 1), ticks_size=7  
-# )
-
-##
 
 # Right
 meta = pd.read_csv(os.path.join(path_data, 'cells_meta.csv'), index_col=0)
@@ -747,4 +710,204 @@ plt.show()
 
 ############## 
 
-# ...
+# Variants biological anno: EDepaquale lab
+afm = read_one_sample(path_data, 'AML_clones')
+variants = pd.Series(filter_baseline(afm).var_names).to_frame('var')
+fun_df = pd.read_csv(path_data + 'rev_table.txt', sep='\t')
+fun_df['var'] = fun_df['Position'].astype('str') + \
+                '_' + fun_df['Reference'] + \
+                '>' + fun_df['Variant'] 
+                
+            
+# Format
+df_annot = fun_df[fun_df['var'].isin(variants['var'])]
+df_annot = (
+    df_annot.loc[:, [
+        'Position', 'Reference', 'Variant', 'Consequence', 'Symbol', \
+        'Biotype', 'SIFT', 'PolyPhen', 'Disease', 'Status'
+    ]]
+    .assign(var=lambda x: 
+       x['Position'].astype('str') + '_' + x['Reference'] + '>' + x['Variant']
+    )
+    .drop(columns=['Position', 'Reference', 'Variant'])
+    .set_index('var')
+)
+
+# Take out positive cells per var info
+df_positive = (
+    pd.Series(np.sum(afm.X > 0, axis=0), index=afm.var_names)
+    .to_frame('n_positive')
+    .assign(variant_type=lambda x: x.index.map(lambda x: x.split('_')[1]))
+    .loc[lambda x: ~x['variant_type'].str.contains('N>')]
+)
+order = (
+    df_positive.groupby('variant_type')
+    .agg('mean')
+    .sort_values('n_positive', ascending=False)
+    .index
+)
+
+# Fig
+fig = plt.figure(figsize=(13, 4))
+gs = GridSpec(1, 3, figure=fig, width_ratios=[5, 8, 3])
+
+# Subs type
+ax = fig.add_subplot(gs[0,0])
+strip(df_positive, 'variant_type', 'n_positive', c='#2e2d2d', s=3, order=order, ax=ax)
+ax.spines[['top', 'right', 'left']].set_visible(False)
+medians = (
+    df_positive.groupby('variant_type')
+    .agg('mean')
+    .sort_values('n_positive', ascending=False)
+    .values
+)
+for i, y in enumerate(medians):
+    ax.hlines(y, i-.25, i+.25, 'r', zorder=4)
+format_ax(ax, title=f'n of +events, by substitution type', ylabel='n cells', rotx=90)
+
+##
+
+# Gene 
+x = 'Symbol'
+ax = fig.add_subplot(gs[0,1])
+_ = df_annot[x].value_counts().to_frame('count')
+bar(_, 'count', c='lightgrey', edgecolor='k', ax=ax)
+format_ax(ax, title='MT-SNVs by genomic feature', ylabel='n variants', xticks=_.index.values, 
+        rotx=90 if _.index.size>5 else 0)
+ax.spines[['top', 'right', 'left']].set_visible(False)
+
+##
+
+# Biotype
+x = 'Biotype'
+ax = fig.add_subplot(gs[0,2])
+_ = df_annot[x].value_counts().to_frame('count')
+bar(_, 'count', c='lightgrey', edgecolor='k', ax=ax)
+format_ax(ax, title='MT-SNVs by biotype', ylabel='n variants', xticks=_.index.values, rotx=90)
+ax.spines[['top', 'right', 'left']].set_visible(False)
+
+# Plot
+fig.tight_layout()
+plt.show()
+
+
+##
+
+
+# Functional anno
+
+# Fig
+fig = plt.figure(figsize=(13, 4))
+gs = GridSpec(1, 3, figure=fig, width_ratios=[7, 5, 5])
+
+# Codon
+_ = df_annot['Consequence'].value_counts().to_frame('count')
+_.index = [
+    'missense',
+    'synonimous',
+    'non_coding',
+    'stop_gain',
+    'incomplete_terminal',
+    'intergenic',
+    'stop_retained'
+]
+
+ax = fig.add_subplot(gs[0,0])
+bar(_, 'count', c='lightgrey', edgecolor='k', ax=ax)
+format_ax(ax, title='MT-SNVs codon effect', ylabel='n variants', xticks=_.index.values, rotx=90)
+ax.spines[['top', 'right', 'left']].set_visible(False)
+
+# SIFT
+_ = df_annot['SIFT'].map(lambda x: x.split('(')[0]).value_counts().to_frame('count')
+ax = fig.add_subplot(gs[0,1])
+bar(_, 'count', c='lightgrey', edgecolor='k', ax=ax)
+format_ax(ax, title='Protein effect (SIFT)', ylabel='n variants', xticks=_.index.values, rotx=90)
+ax.spines[['top', 'right', 'left']].set_visible(False)
+
+# Polyphen
+_ = df_annot['PolyPhen'].map(lambda x: x.split('(')[0]).value_counts().to_frame('count')
+ax = fig.add_subplot(gs[0,2])
+bar(_, 'count', c='lightgrey', edgecolor='k', ax=ax)
+format_ax(ax, title='Protein effect (PolyPhen)', ylabel='n variants', xticks=_.index.values, rotx=90)
+ax.spines[['top', 'right', 'left']].set_visible(False)
+
+# Plot
+fig.tight_layout()
+plt.show()
+
+##
+
+##############
+
+
+##
+
+
+############## 
+
+# Are homoplasmic, high-prevalence variants under active selection??
+var_prevalence = np.sum(afm.X>0, axis=0) / afm.shape[0]
+var_mean_AF = np.nanmean(afm.X, axis=0)
+df_ = (
+    pd.DataFrame(
+        {'prevalence' : var_prevalence, 'mean_AF': var_mean_AF},
+        index=afm.var_names
+    )
+    .assign(type=lambda x: np.where((x['prevalence']>0.5) & (x['mean_AF']>0.5), 'high', 'low'))
+    .join(df_annot.loc[:, ['Consequence', 'SIFT', 'PolyPhen']], how='inner') 
+    # NB. Make sure df_annot is filtered!
+)
+n_high = df_.query('type == "high"').shape[0]
+
+##
+
+# Plot
+fig = plt.figure(figsize=(10, 5))
+gs = GridSpec(1, 2, figure=fig, width_ratios=[2,2])
+
+# Biplot mean_AF/prevalence
+ax = fig.add_subplot(gs[0,0])
+ax.plot(df_.query('type == "low"')['mean_AF'], df_.query('type == "low"')['prevalence'], 'k.', markersize=3)
+ax.plot(df_.query('type == "high"')['mean_AF'], df_.query('type == "high"')['prevalence'], 'r+', markersize=6)
+format_ax(ax, xlabel='Mean AF', ylabel='Prevalence')
+
+ax.text(.4, .2, f'MT-SNVs:', transform=ax.transAxes)
+ax.text(.4, .15, f'-{df_.shape[0]} passing baseline filters', transform=ax.transAxes)
+ax.text(.4, .1, f'-{n_high} >0.5 mean_AF and prevalence',  transform=ax.transAxes)
+ax.spines[['right', 'top']].set_visible(False)
+
+# Lollipop consequence
+ax = fig.add_subplot(gs[0,1])
+stem_plot(df_.query('type == "high"').sort_values('mean_AF', ascending=False), 'mean_AF', ax=ax)
+format_ax(ax, xlabel='Mean AF')
+ax.spines[['right', 'top', 'left']].set_visible(False)
+
+tests = [
+    df_['Consequence'] == 'non_coding_transcript_exon_variant',
+    df_['Consequence'] == 'synonymous_variant',
+    df_['Consequence'] == 'missense_variant'
+]
+df_['codon'] = np.select(tests, ['non coding', 'synonimous', 'missense'])
+annot_list = df_.query('type == "high"').loc[:, ['codon', 'PolyPhen']].values.tolist()
+
+_ = .97
+i = 0
+vep, poly = annot_list[i]
+#print(i, vep, poly)
+ax.text(.1, _, f'VEP: {vep}; PolyPhen: {poly}', transform=ax.transAxes)
+for x in annot_list[1:]:
+    i += 1
+    vep, poly = x
+    #print(i, vep, poly)
+    ax.text(.1, _-(0.1*i), f'VEP: {vep}; PolyPhen: {poly}', transform=ax.transAxes)
+
+fig.tight_layout()
+plt.show()
+
+##############
+
+
+##
+
+
+############## 
---
+++ bin/top_models.py
@@ -0,0 +1,282 @@
+#!/usr/bin/python
+
+# Top models retraining, diagnostics and feature importance
+
+########################################################################
+
+# Libraries
+import argparse
+import sys
+import argparse
+import pickle
+from mito_utils.utils import *
+from mito_utils.preprocessing import *
+from mito_utils.dimred import *
+from mito_utils.classification import *
+
+# Create the parser
+my_parser = argparse.ArgumentParser(
+    prog='Top models evaluation',
+    description=
+        """
+        Systematically testing the ability of (filtered) MT-SNVs to distinguish
+        ground truth clonal labels from lentiviral barcoding.
+        """
+)
+
+# Add arguments
+
+# Path_main
+my_parser.add_argument(
+    '-p', 
+    '--path_main', 
+    type=str,
+    default='..',
+    help='Path to samples data. Default: .. .'
+)
+
+# Filter
+my_parser.add_argument(
+    '--sample', 
+    type=str,
+    default='MDA_clones',
+    help='Sample to use. Default: MDA_clones.'
+)
+
+# ncombos
+my_parser.add_argument(
+    '--ncombos', 
+    type=int,
+    default=100,
+    help='n combinations of hyperparameters to test. Default: 100.'
+)
+
+# ntop
+my_parser.add_argument(
+    '--ntop', 
+    type=int,
+    default=3,
+    help='n top models to evalutate. Default: 3.'
+)
+
+# ncores
+my_parser.add_argument(
+    '--ncores', 
+    type=int,
+    default=8,
+    help='ncores to use for model training. Default: 8.'
+)
+
+# Score
+my_parser.add_argument(
+    '--score', 
+    type=str,
+    default='f1',
+    help='Classification performance scoring type. Default: f1-score.'
+)
+
+# Parse arguments
+args = my_parser.parse_args()
+
+##
+
+# Args
+# path_main = '/Users/IEO5505/Desktop/example_mito/'
+# sample = 'AML_clones'
+# ncores = 8
+# score = 'f1'
+# n_combos = 5
+# n = 3
+
+path_main = args.path_main
+sample =  args.sample
+ncores = args.ncores
+score = args.score
+n_combos = args.ncombos
+n = args.ntop
+
+# Paths
+path_data = os.path.join(path_main, 'data')
+path_results = os.path.join(path_main, 'results/supervised_clones/top_models/')
+path_clones = os.path.join(path_main, 'results/supervised_clones/reports/report_f1.csv')
+
+##
+
+########################################################################
+
+def main():
+    
+    # Logger
+    logger = set_logger(
+        path_results, 
+        f'log_top_{sample}.txt')
+    )
+    
+    logger.info(
+        f""" 
+        Execute top models final evaluation: \n
+        --sample {sample} 
+        --ncombos {n_combos} 
+        --score {score}
+        --ntop {n}
+        """
+    )
+    
+    T = Timer()
+    T.start()
+
+    # Data    
+    afm = read_one_sample(path_data, sample=sample)
+    
+    # Get models 
+    clones = pd.read_csv(path_clones, index_col=0)
+    top_models = (
+        clones
+        .query('sample == @sample')
+        .assign(job=lambda x: x['filtering'] + '|' + x['dimred'] + '|' + x['model'] + '|' + x['tuning'])
+        .groupby('job')
+        .agg('mean')
+        .sort_values('f1', ascending=False)
+        .head(n)
+    )
+    top_models = top_models.iloc[:, 5:]
+    
+    top_dict = { }
+    for i in range(n):
+        s = top_models.iloc[i,:]
+        filtering, dimred, model, tuning = s.name.split('|')
+        d_model = {
+            'filtering' : filtering,
+            'dimred' : dimred,
+            'model' : model,
+            'tuning' : tuning,
+            'min_cell_number' : s['min_cell_number'],
+            'min_cov_treshold' : s['min_cov_treshold']
+        }
+        top_dict[f'top_{i+1}_{sample}'] = d_model
+        
+        
+    ##
+    
+    
+    # Here we go
+    for job in top_dict:
+        
+        # Get options
+        model_options = top_dict[model]
+        filtering = model_options['filtering']
+        dimred = model_options['dimred']
+        min_cell_number = model_options['min_cell_number']
+        min_cov_treshold = model_options['min_cov_treshold']
+        model = model_options['model']
+        GS_mode = model_options['tuning']
+        
+        # Filters
+        if dimred == 'no_dimred':
+
+            _, a = filter_cells_and_vars(
+                afm,
+                # blacklist=blacklist,
+                sample=sample,
+                filtering=filtering, 
+                min_cell_number=min_cell_number, 
+                min_cov_treshold=min_cov_treshold, 
+                nproc=ncores, 
+                path_=path
+            )
+
+            # Extract X, y
+            a = nans_as_zeros(a) # For sklearn APIs compatibility
+            ncells = a.shape[0]
+            n_clones_analyzed = len(a.obs['GBC'].unique())
+            X = a.X
+            y = pd.Categorical(a.obs['GBC'])
+            Y = one_hot_from_labels(y)
+
+        else:
+
+            _, a = filter_cells_and_vars(
+                afm,
+                sample=sample,
+                filtering=filtering, 
+                min_cell_number=min_cell_number, 
+                min_cov_treshold=min_cov_treshold, 
+                nproc=ncores
+            )
+
+            # Extract X, y
+            a = nans_as_zeros(a) # For sklearn APIs compatibility
+            ncells = a.shape[0]
+            n_clones_analyzed = len(a.obs['GBC'].unique())
+            X, _ = reduce_dimensions(a, method=dimred, n_comps=30, sqrt=False)
+            y = pd.Categorical(a.obs['GBC'])
+            Y = one_hot_from_labels(y)
+
+        # One per clone
+        
+        L_performance = []
+        d_results = {}
+        
+        for i in range(Y.shape[1]):  
+            
+            t = Timer()
+            t.start()
+            
+            comparison = f'{y.categories[i]}_vs_rest'
+            y_ = Y[:,i]
+            
+            logger.info(f'Starting comparison {comparison} ({i+1}/{Y.shape[1]})...')
+
+            # Classification
+            results = classification(
+                X, y_, key=model, GS=True, GS_mode='random', # GS_mode
+                score=score, n_combos=n_combos, cores_model=ncores, cores_GS=1,
+                full_output=True, feature_names=a.var_names
+            )
+            
+            # Performance dict
+            results['performance_dict'] |= {
+                'sample' : sample,
+                'filtering' : filtering, 
+                'dimred' : dimred,
+                'min_cell_number' : min_cell_number,
+                'min_cov_treshold' : min_cov_treshold,
+                'ncells_clone' : y_.sum(),
+                'ncells_sample' : ncells,
+                'n_clones_analyzed' : n_clones_analyzed,
+                'n_features' : X.shape[1],
+                'model' : model,
+                'score_for_tuning' : score,
+                'comparison' : comparison
+            }         
+            L_performance.append(results['performance_dict'])
+            
+            # Results
+            d_results[comparison] = results
+            logger.info(f'Finished {comparison} ({i+1}/{Y.shape[1]}): {t.stop()}')
+            
+        ##
+            
+        # Save all results
+        df = pd.DataFrame(L_performance)
+        print(df['f1'].describe())
+            
+        # Save df_performance
+        df.to_csv(
+            os.path.join(
+                path_results, 
+                f'out_top_{job}_{sample}.csv'
+            )
+        )
+        
+        # Save pickled
+        with open(os.path.join(path_results, f'results_top_{job}_{sample}.pkl'), 'wb') as f:
+            pickle.dump(d_results, f)
+            
+#######################################################################
+
+# Run program
+if __name__ == "__main__":
+    main()
+
+#######################################################################
