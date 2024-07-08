66d44154171f8331851eecfb0d6d2a5ba3eddbca
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2024-07-08 15:00:46 +0200

    change path outs

--- downstream/launch.py
+++ downstream/launch.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 
 import os
+import json
 from itertools import product
+from mito_utils.utils import *
 
-combos = list(product(['MDA_clones', 'MDA_clones_old'], ['stringent', 'enriched']))
 
+path_main = '/data/cossa2_dare/MI_TO_benchmark/'
+path_filtering = '/data/cossa2_dare/MI_TO_benchmark/data/filtering_options.json'
+samples = ['MDA_clones', 'AML_clones', 'MDA_PT', 'MDA_lung']
+with open(path_filtering, 'r') as file:
+    FILTERING_OPTIONS = json.load(file)
+filtering_keys = FILTERING_OPTIONS.keys()
+
+# Run
+combos = list(product(samples, filtering_keys))
 for sample, filtering in combos:
-    os.system(f'python multi_classification.py {sample} {filtering} 0.05')
+    os.system(f'python multi_classification.py {path_main} {sample} {filtering} 0.05')
